f0fb7f2b58585d766d17a256ab7efef033160dd7
francomattt <franco.pirondo@gmail.com>
2022-03-13 00:13:21 -0300

    Se agrego una cosita

--- src/main/java/com/example/demo/HardcodeandoPokemonesH2.java
+++ src/main/java/com/example/demo/HardcodeandoPokemonesH2.java
@@ -9,13 +9,17 @@ import com.example.demo.dominio.Pokemon;
 
 @Component
 public class HardcodeandoPokemonesH2 implements CommandLineRunner {
-	
+	/*
+	 * Esta clase es utilizada para hardcodear pokemones en la tabla de datos configurada previamente en el
+	 * application context. Por lo general se utiliza con h2 ya que es una base de datos volatil. 
+	 * 
+	 */
 	PokemonDao pokemonDao;
 
 	@Override
 	public void run(String... args) throws Exception {
 		Pokemon p = new Pokemon("Pikachu", "Electrico");
-		pokemonDao.save(p);
+		/*pokemonDao.save(p);
 		pokemonDao.save(new Pokemon("Charmander", "Fuego"));
 		pokemonDao.save(new Pokemon("Squirtle", "Agua"));
 		pokemonDao.save(new Pokemon("Lapras", "Agua"));
@@ -24,6 +28,7 @@ public class HardcodeandoPokemonesH2 implements CommandLineRunner {
 		pokemonDao.save(new Pokemon("Dragonite", "Dragon"));
 		pokemonDao.save(new Pokemon("Magnemite", "Electrico"));
 		System.out.println("pokemon hardcodeado");
+		*/
 		
 	}
 
